63e4b90a013978699b6b5290035cd92407712a02
raymo <raymo@DESKTOP-VIK2O2O>
2021-04-06 23:36:57 +0800

    Exercise2.2

--- Exercise2.2/src/main.java
+++ Exercise2.2/src/main.java
@@ -3,7 +3,6 @@ public class main {
 	public static void main(String[] args) {
 		WashingMachines a = new WashingMachines();
 		String [] functionAndCapacity_A = {"Regular", "Quick", "Water Saving", "Delicates"};
-		int rsA1=35,rsA2=40,rsA3=25,rsA4=35;
 		a.functionAndCapacity = functionAndCapacity_A;
 		a.printWashingMachines("Toshiba", "Black", 41, 10);
 		a.printOnOff(true);
@@ -12,7 +11,6 @@ public class main {
 		
 		WashingMachines b = new WashingMachines();
 		String [] functionAndCapacity_B = {"Regular", "Quick", "Water Saving", "Delicates"};
-		int rsB1=40, rsB2=45, rsB3=30, rsB4=55;
 		a.functionAndCapacity = functionAndCapacity_B;
 		a.printWashingMachines("LG", "White", 50, 12);
 		a.printOnOff(true);
